284d57050487a8cd42a613f1e8d0fc8ac1b0f792
M-Sayer <haji.san@icloud.com>
2020-08-23 15:02:19 -0400

    maze runner solved all solutions

--- Recursion/mazeRunner.js
+++ Recursion/mazeRunner.js
@@ -1,58 +1,51 @@
-let smallMaze = [
-  [' ', '*', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', 'e']
-]
-
 function solveMaze(maze) {
-  let visited = maze
-  let currentPath = []
-  let allPaths = []
-
-  function checkMove(maze, visited, row, column) {
-    if((row >= 0) && (row < maze.length) && (column >= 0) && (column < maze[row].length) && (maze[row][column] !== '*') && (visited[row][column] !== '-')) {
-      return true 
-    }
-    return false
-  }
-  
+  let visited = maze;
+  const paths = [];
+  let route = [];
   function mazeRunner(maze, visited, row, column) {
-    const directionRow = [0,0,-1,1] // left, right, up, down
-    const directionColumn = [-1,1,0,0] // left, right, up, down
-    const directions = ['L', 'R', 'U', 'D']
-  
-    //base case 
-    if (maze[row][column] === 'e') {
+    const directionRow = [0, 0, -1, 1]; // left, right, up, down
+    const directionColumn = [-1, 1, 0, 0]; // left, right, up, down
+    const directions = ["L", "R", "U", "D"];
+    
+    function checkMove(maze, visited, row, column) {
+      if (
+        row >= 0 &&
+        row < maze.length &&
+        column >= 0 &&
+        column < maze[row].length &&
+        maze[row][column] !== "*" &&
+        visited[row][column] !== "-"
+      ) {
+        return true;
+      }
+      return false;
+    }
 
-      allPaths.push(currentPath.toString().replace(/,/g, ''))
-      currentPath = []
-
-    } else {
-      //this console log is showing the program gets wonky when it hits a dead end
-      console.log(currentPath)
+    //base case
+    if (maze[row][column] === "e") {
+      paths.push(route.join("")); //add route to successful paths
+    } else { // recursive case
+      visited[row][column] = "-";
       for (let i = 0; i < directionRow.length; i++) {
-        const nextRow = row + directionRow[i]
-        const nextColumn = column + directionColumn[i]
-        
-        visited[row][column] = '-'
-
-        if(checkMove(maze, visited, nextRow, nextColumn)) {
-          currentPath.push(directions[i])
-          console.log(currentPath)
-
-          console.log(visited)
-          
-          mazeRunner(maze, visited, nextRow, nextColumn)
-          
-        } else if (!checkMove(maze, visited, nextRow, nextColumn)) {
-          // do something here ?
+        const nextRow = row + directionRow[i];
+        const nextColumn = column + directionColumn[i];
+        if (checkMove(maze, visited, nextRow, nextColumn)) {
+          route.push(directions[i]); // put the direction on the current path
+          mazeRunner(maze, visited, nextRow, nextColumn);
+          route.pop(); // take the direction off the current path
         }
       }
+      visited[row][column] = ' '
     }
+    return paths;
   }
-  mazeRunner(maze, visited, 0, 0)
-  return allPaths
+  return mazeRunner(maze, visited, 0, 0);
 }
 
-console.log(solveMaze(smallMaze))
+let smallMaze = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', '*', 'e']
+]
 
+console.log(solveMaze(smallMaze))
